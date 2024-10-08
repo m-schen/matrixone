47108eff5e01c1cbbe07ac8041711dcbcff90c4d
chenmingsong <mschenjj@foxmail.com>
2024-10-08 14:23:45 +0800

    support storeBatch

--- pkg/sql/colexec/hashcms/types.go
+++ pkg/sql/colexec/hashcms/types.go
@@ -15,10 +15,12 @@
 package hashcms
 
 import (
-    "github.com/matrixorigin/matrixone/pkg/common/hashmap"
-    "github.com/matrixorigin/matrixone/pkg/container/batch"
-    "github.com/matrixorigin/matrixone/pkg/container/vector"
-    "runtime"
+	"github.com/matrixorigin/matrixone/pkg/common/hashmap"
+	"github.com/matrixorigin/matrixone/pkg/container/batch"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+	"runtime"
 )
 
 // spilledHashMap splits and spills the data sources of the hash map to the disk.
@@ -26,43 +28,45 @@ import (
 // hashInfo: information for hash map creation.
 // spilledBatchInfo: information for spilled file.
 type spilledHashMap struct {
-    hashInfo struct {
-        isStrHashMap bool
-        keyHasNulls  bool
+	hashInfo struct {
+		isStrHashMap bool
+		keyHasNulls  bool
 
-        // hashOnUniqueColumn is true for hash on primary key and other columns with unique index.
-        hashOnUniqueColumn bool
+		// hashOnUniqueColumn is true for hash on primary key and other columns with unique index.
+		hashOnUniqueColumn bool
 
-        // keyColumnIdxList indicates which columns were hash table keys.
-        keyColumnIdxList []int
-    }
+		// totalColumnCount indicates how many vectors should be stored for one spilledBatch.
+		totalColumnCount int
+		// keyColumnIdxList indicates which columns were hash table keys.
+		keyColumnIdxList []int
+	}
 
-    blocks []spilledBatchInfo
+	blocks []spilledBatchInfo
 }
 
 // spilledBatchInfo stores all information about one spilled batch.
 type spilledBatchInfo struct {
-    path string
-    rowCount uint64
+	path     string
+	rowCount uint64
 }
 
 type hashmapBuildingContext struct {
-    // doPreAllocatedCheck true means should do specific check during insert data.
-    doPreAllocatedCheck bool
-    // requireInputRows is how many rows of data should insert.
-    requireInputRows uint64
-    // alreadyInputRows is how many rows of data has been insert into hashmap.
-    alreadyInputRows int
+	// doPreAllocatedCheck true means should do specific check during insert data.
+	doPreAllocatedCheck bool
+	// requireInputRows is how many rows of data should insert.
+	requireInputRows uint64
+	// alreadyInputRows is how many rows of data has been insert into hashmap.
+	alreadyInputRows int
 }
 
 // readBatchByPath read a batch according to its disk path.
 func readBatchByPath(str string) *batch.Batch {
-    return nil
+	return nil
 }
 
 // writeBatchToFileService write a batch to disk.
 func writeBatchToFileService(data *batch.Batch) (filePath string, err error) {
-    return
+	return
 }
 
 // removeBatchByPath remove a batch from disk.
@@ -74,12 +78,43 @@ func removeBatchByPath(str string) {
 // 1. create a new dst batch to avoid modifying the src batch.
 // 2. evaluate the keys of hash map, and save them to the dst batch.
 // 3. spill the dst batch to the disk.
+// 4. add spill-batch information to the spilledHashMap.
 func (spilledHm *spilledHashMap) StoreBatch(
-    src *batch.Batch) error {
+	proc *process.Process, src *batch.Batch, keyExecutors ...colexec.ExpressionExecutor) error {
 
+	dst := &batch.Batch{
+		Recursive:  src.Recursive,
+		Attrs:      src.Attrs,
+		ShuffleIDX: src.ShuffleIDX,
+		Vecs:       make([]*vector.Vector, 0, spilledHm.hashInfo.totalColumnCount),
+	}
+	dst.SetRowCount(src.RowCount())
+	dst.Vecs = append(dst.Vecs, src.Vecs...)
 
+	for i, idx := range spilledHm.hashInfo.keyColumnIdxList {
+		if idx < len(src.Vecs) {
+			dst.Vecs = append(dst.Vecs, src.Vecs[idx])
+			continue
+		}
 
-    return nil
+		// if the key column is not an origin vector.
+		v, e := keyExecutors[i].Eval(proc, []*batch.Batch{src}, nil)
+		if e != nil {
+			return e
+		}
+		dst.Vecs = append(dst.Vecs, v)
+	}
+
+	path, err := writeBatchToFileService(dst)
+	if err != nil {
+		return err
+	}
+
+	spilledHm.blocks = append(spilledHm.blocks, spilledBatchInfo{
+		path:     path,
+		rowCount: uint64(dst.RowCount()),
+	})
+	return nil
 }
 
 // ReadHashMapByIdxes return a hashmap from specific batches.
@@ -87,71 +122,71 @@ func (spilledHm *spilledHashMap) StoreBatch(
 // 1. build an empty hash map.
 // 2. loop to read batch and insert into hash map.
 func (spilledHm *spilledHashMap) ReadHashMapByIdxes(idxes ...int) (hashmap.HashMap, error) {
-    hmp, hmpItr, err := buildEmptyHashMap(spilledHm.hashInfo.isStrHashMap, spilledHm.hashInfo.keyHasNulls)
-    if err != nil {
-        return nil, err
-    }
+	hmp, hmpItr, err := buildEmptyHashMap(spilledHm.hashInfo.isStrHashMap, spilledHm.hashInfo.keyHasNulls)
+	if err != nil {
+		return nil, err
+	}
 
-    insertContext := &hashmapBuildingContext{
-        requireInputRows: 0,
-        alreadyInputRows: 0,
-    }
+	insertContext := &hashmapBuildingContext{
+		requireInputRows: 0,
+		alreadyInputRows: 0,
+	}
 
-    for _, idx := range idxes {
-        insertContext.requireInputRows += spilledHm.blocks[idx].rowCount
-    }
+	for _, idx := range idxes {
+		insertContext.requireInputRows += spilledHm.blocks[idx].rowCount
+	}
 
-    // If hash on a unique column, total row count is the final size of hash map.
-    //
-    // we allocated memory for hash map to avoid memory expand while appending data.
-    if spilledHm.hashInfo.hashOnUniqueColumn {
-        if err = hmp.PreAlloc(insertContext.requireInputRows); err != nil {
-            hmp.Free()
-            return nil, err
-        }
-    }
+	// If hash on a unique column, total row count is the final size of hash map.
+	//
+	// we allocated memory for hash map to avoid memory expand while appending data.
+	if spilledHm.hashInfo.hashOnUniqueColumn {
+		if err = hmp.PreAlloc(insertContext.requireInputRows); err != nil {
+			hmp.Free()
+			return nil, err
+		}
+	}
 
-    // If not hash on unique column, and this may not a very small hash table.
-    insertContext.doPreAllocatedCheck = !spilledHm.hashInfo.hashOnUniqueColumn &&
-        insertContext.requireInputRows > hashmap.HashMapSizeThreshHold
+	// If not hash on unique column, and this may not a very small hash table.
+	insertContext.doPreAllocatedCheck = !spilledHm.hashInfo.hashOnUniqueColumn &&
+		insertContext.requireInputRows > hashmap.HashMapSizeThreshHold
 
-    // data insert.
-    for _, idx := range idxes {
-        b := readBatchByPath(spilledHm.blocks[idx].path)
-        if err = spilledHm.insertBatchIntoHashmap(hmp, hmpItr, b, insertContext); err != nil {
-            hmp.Free()
-            return nil, err
-        }
+	// data insert.
+	for _, idx := range idxes {
+		b := readBatchByPath(spilledHm.blocks[idx].path)
+		if err = spilledHm.insertBatchIntoHashmap(hmp, hmpItr, b, insertContext); err != nil {
+			hmp.Free()
+			return nil, err
+		}
 
-        // todo: 怎么处理b.
-    }
-    return hmp, nil
+		// todo: 怎么处理b.
+	}
+	return hmp, nil
 }
 
 // buildEmptyHashMap generate an empty hash map.
 func buildEmptyHashMap(isStr bool, hasNulls bool) (hashmap.HashMap, hashmap.Iterator, error) {
-    var hmp hashmap.HashMap
-    var hmpItr hashmap.Iterator
+	var hmp hashmap.HashMap
+	var hmpItr hashmap.Iterator
 
-    if isStr {
-        strHmp, err := hashmap.NewStrMap(hasNulls)
-        if err != nil {
-            return nil, nil, err
-        }
+	if isStr {
+		strHmp, err := hashmap.NewStrMap(hasNulls)
+		if err != nil {
+			return nil, nil, err
+		}
 
-        hmpItr = strHmp.NewIterator()
-        hmp = strHmp
-    } else {
-        intHmp, err := hashmap.NewIntHashMap(hasNulls)
-        if err != nil {
-            return nil, nil, err
-        }
+		hmpItr = strHmp.NewIterator()
+		hmp = strHmp
+	} else {
+		intHmp, err := hashmap.NewIntHashMap(hasNulls)
+		if err != nil {
+			return nil, nil, err
+		}
 
-        hmpItr = intHmp.NewIterator()
-        hmp = intHmp
-    }
+		hmpItr = intHmp.NewIterator()
+		hmp = intHmp
+	}
 
-    return hmp, hmpItr, nil
+	return hmp, hmpItr, nil
 }
 
 // insertBatchIntoHashmap upsert the hash map with input data.
@@ -159,53 +194,53 @@ func buildEmptyHashMap(isStr bool, hasNulls bool) (hashmap.HashMap, hashmap.Iter
 // 1. try to expand the memory.
 // 2. do insertion.
 func (spilledHm *spilledHashMap) insertBatchIntoHashmap(
-    hmp hashmap.HashMap,
-    itr hashmap.Iterator,
-    data *batch.Batch, ctx *hashmapBuildingContext) error {
+	hmp hashmap.HashMap,
+	itr hashmap.Iterator,
+	data *batch.Batch, ctx *hashmapBuildingContext) error {
 
-    thisLength := data.Vecs[0].Length()
+	thisLength := data.Vecs[0].Length()
 
-    keys := make([]*vector.Vector, len(spilledHm.hashInfo.keyColumnIdxList))
+	keys := make([]*vector.Vector, len(spilledHm.hashInfo.keyColumnIdxList))
 
-    for i := 0; i < thisLength; i += hashmap.UnitLimit {
-        if i % (hashmap.UnitLimit * 32) == 0 {
-            runtime.Gosched()
-        }
+	for i := 0; i < thisLength; i += hashmap.UnitLimit {
+		if i%(hashmap.UnitLimit*32) == 0 {
+			runtime.Gosched()
+		}
 
-        n := thisLength - i
-        if n > hashmap.UnitLimit {
-            n = hashmap.UnitLimit
-        }
+		n := thisLength - i
+		if n > hashmap.UnitLimit {
+			n = hashmap.UnitLimit
+		}
 
-        // do estimation for hashmap size and do pre-allocation after 8192 rows.
-        if ctx.doPreAllocatedCheck {
-            if ctx.alreadyInputRows >= hashmap.HashMapSizeEstimate {
+		// do estimation for hashmap size and do pre-allocation after 8192 rows.
+		if ctx.doPreAllocatedCheck {
+			if ctx.alreadyInputRows >= hashmap.HashMapSizeEstimate {
 
-                group := hmp.GroupCount()
-                rate := float64(group) / float64(ctx.alreadyInputRows)
-                mayCap := uint64(float64(ctx.requireInputRows) * rate)
-                if mayCap > group {
-                    if err := hmp.PreAlloc(mayCap - group); err != nil {
-                        return err
-                    }
-                }
+				group := hmp.GroupCount()
+				rate := float64(group) / float64(ctx.alreadyInputRows)
+				mayCap := uint64(float64(ctx.requireInputRows) * rate)
+				if mayCap > group {
+					if err := hmp.PreAlloc(mayCap - group); err != nil {
+						return err
+					}
+				}
 
-                ctx.doPreAllocatedCheck = false
-            }
-        }
+				ctx.doPreAllocatedCheck = false
+			}
+		}
 
-        // insertion.
-        for j := range keys {
-            keys[j] = data.Vecs[spilledHm.hashInfo.keyColumnIdxList[j]]
-        }
-        _, _, err := itr.Insert(i, n, keys)
-        if err != nil {
-            return err
-        }
+		// insertion.
+		for j := range keys {
+			keys[j] = data.Vecs[spilledHm.hashInfo.keyColumnIdxList[j]]
+		}
+		_, _, err := itr.Insert(i, n, keys)
+		if err != nil {
+			return err
+		}
 
-        // update the already input count.
-        ctx.alreadyInputRows += n
-    }
+		// update the already input count.
+		ctx.alreadyInputRows += n
+	}
 
-    return nil
+	return nil
 }
